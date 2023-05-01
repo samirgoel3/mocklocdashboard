2f8110f5d410c989525f1e6a0d6bea7c8253a8d4
Samir Goel <samirgoel3@gmail.com>
2023-05-01 18:21:53 +0530

    sonar test

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,3 @@
-// import 'react-native';
-// import React from 'react';
-// import App from '../App';
-
-// // Note: test renderer must be required after react-native.
-// import renderer from 'react-test-renderer';
-
-// it('renders correctly', () => {
-//   renderer.create(<App />);
-// });
-
 import {addition} from '../src/util/Mathmatics';
 
 test("ADDITION TESTING", ()=>{
